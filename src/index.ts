862a7b1851fea44555cd3946b048ffcd6e622603
gp <512167281@qq.com>
2024-03-19 17:42:07 +0800

    npm has publish

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,37 @@
-import Decimal from "decimal.js";
+import Decimal from 'decimal.js';
 
-const zhFractions = ["角", "分"];
+const zhFractions = ['角', '分'];
 
-const zhDigits = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"];
+const zhDigits = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
 
 // 大单位
 const zhUnits = [
-  "", // 10^0
-  "万", // 10^4
-  "亿", // 10^8
-  "兆", // 10^12
-  "京", // 10^16
-  "垓", // 10^20
-  "秭", // 10^24
-  "穰", // 10^28
-  "沟", // 10^32
-  "涧", // 10^36
-  "正", // 10^40
-  "载", // 10^44
-  "极", // 10^48
-  "恒河沙", // 10^52
-  "阿僧祗", // 10^56
-  "那由他", // 10^60
-  "不可思议", // 10^64
-  "无量", // 10^68
-  "大数", // 巨大数目
+  '', // 10^0
+  '万', // 10^4
+  '亿', // 10^8
+  '兆', // 10^12
+  '京', // 10^16
+  '垓', // 10^20
+  '秭', // 10^24
+  '穰', // 10^28
+  '沟', // 10^32
+  '涧', // 10^36
+  '正', // 10^40
+  '载', // 10^44
+  '极', // 10^48
+  '恒河沙', // 10^52
+  '阿僧祗', // 10^56
+  '那由他', // 10^60
+  '不可思议', // 10^64
+  '无量', // 10^68
+  '大数', // 巨大数目
 ];
 
 // 最大到那由他
 const limitNum = new Decimal(10).toPower(64);
 
 // 局部单位
-const unitl = ["", "拾", "佰", "仟"];
+const unitl = ['', '拾', '佰', '仟'];
 
 interface LocalResult {
   num: number;
@@ -124,11 +124,11 @@ function spell(localStack: LocalResult[]) {
     words = zhDigits[0];
   } else {
     words = localStack
-      .map((e) => {
+      .map(e => {
         return e.num !== 0 ? `${e.numHan}${e.unit}` : `${e.numHan}`;
       })
       .reverse()
-      .join("");
+      .join('');
   }
   return words;
 }
@@ -195,13 +195,13 @@ function parseIntegerPart(integerPart: Decimal) {
     if (lastThunk) {
       if (lastThunk.unitId - aThunk.unitId > 1) {
         // 要加0，因为上一个空的被跳过了
-        allWords.push("零");
+        allWords.push('零');
       } else {
         // 如果是相邻的，则要判断当前的最高位是否为空
         // 能到达这里说明当前的最高位应该是千位，
         // 因为能到这里说明肯定在更高的位置上有更大的单位阿斯顿发山东发生的
         if (last(aThunk.localResult).num === 0) {
-          allWords.push("零");
+          allWords.push('零');
         }
       }
     }
@@ -218,8 +218,8 @@ function parseIntegerPart(integerPart: Decimal) {
 }
 
 function spellFraction(result: any[]) {
-  const words = result.map((e) => {
-    if (e.num === 0) return "";
+  const words = result.map(e => {
+    if (e.num === 0) return '';
     return `${e.numHan}${e.unit}`;
   });
   return words;
@@ -232,7 +232,6 @@ function parseDecimalPart(decimalPart: Decimal) {
 
   let number = decimalPart;
   for (let i = 0; i < zhFractions.length; i++) {
-    const x = `${number}`;
     number = number.mul(10);
 
     const remainder = number.mod(10).floor();
@@ -254,15 +253,14 @@ function parseDecimalPart(decimalPart: Decimal) {
   return { allWords, thunks, isZero };
 }
 
-// group span
-function rmb(value: string) {
+function dxrmb(value: string | number) {
   let sign = Decimal.sign(value);
 
   // 负数后面在考虑
   let number = Decimal.abs(value);
 
   if (number.cmp(limitNum) >= 0) {
-    throw new Error("超过最大支持10的64次方");
+    throw new Error('超过最大支持10的64次方');
   }
 
   // 获取整数部分
@@ -276,32 +274,34 @@ function rmb(value: string) {
   const words: string[] = [];
   // 读元
   if (!integerPart.isZero()) {
-    words.push(integer.allWords.join(""), "元");
+    words.push(integer.allWords.join(''), '元');
   }
 
   if (decimal.thunks[0].num !== 0) {
-    words.push(decimal.thunks[0].numHan, "角");
+    words.push(decimal.thunks[0].numHan, '角');
   }
 
   if (decimal.thunks[1].num !== 0) {
     // if (integerPart !== 0 && decimal.thunks[0].num === 0) {
     //   words.push("零");
     // }
-    words.push(decimal.thunks[1].numHan, "分");
+    words.push(decimal.thunks[1].numHan, '分');
   }
 
   if (words.length === 0) {
-    return "零元整";
+    return '零元整';
   }
 
   // 如果有元而且没分就以整结尾
   if (!integerPart.isZero() && decimal.thunks[1].num === 0) {
-    words.push("整");
+    words.push('整');
   }
 
   if (sign < 0) {
-    words.unshift("负");
+    words.unshift('负');
   }
 
-  return words.join("");
+  return words.join('');
 }
+
+export default dxrmb;
